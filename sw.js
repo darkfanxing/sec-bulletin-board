51bd67e28310aedf81e96a71c528b68a8d25ee74
darkfanxing <darkfanxing@gmail.com>
2021-01-20 01:00:12 +0800

    PWA 7

--- sw.js
+++ sw.js
@@ -1,35 +1,22 @@
-self.addEventListener('install', function(event) {
-    self.skipWaiting();
-    
-    var offlinePage = new Request('offline.html');
-    event.waitUntil(
-    fetch(offlinePage).then(function(response) {
-      return caches.open('offline2').then(function(cache) {
-        return cache.put(offlinePage, response);
-      });
-    }));
-  }); self.addEventListener('fetch', function(event) {
-    event.respondWith(
-      fetch(event.request).catch(function(error) {
-          return caches.open('offline2').then(function(cache) {
-            return cache.match('offline.html');
-        });
-      }));
-  }); self.addEventListener('refreshOffline', function(response) {
-    return caches.open('offline2').then(function(cache) {
-      return cache.put(offlinePage, response);
-    });
-  }); self.addEventListener('push', function (event) {
-    var data = event.data.json();   var opts = {
-      body: data.body,
-      icon: data.icon,
-      data: {
-        url: data.url
+var cacheName = 'helloWorld'
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll([
+      'index.html'
+    ]))
+  )
+})
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+    .then(function (response) {
+      if (response) {
+        return response;
       }
-    };
-    event.waitUntil(self.registration.showNotification(data.title, opts));
-  }); self.addEventListener('notificationclick', function(event) {
-    var data = event.notification.data;   event.notification.close();   event.waitUntil(
-      clients.openWindow(data.url)
-    );
-  });
+      return fetch(event.request);
+    })
+  )
+})
